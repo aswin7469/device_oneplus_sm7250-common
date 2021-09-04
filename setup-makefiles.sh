ab70a4e78fd39cfc4d1523038ea964176d9f125a
aswin7469 <aswinas@pixysos.com>
2021-09-04 16:38:25 +0530

    sm7250-common: decommonise to avicii
    * merged avicii device tree up to https://github.com/PixelExperience-Devices/device_oneplus_avicii@ce921737fe5ceaa794b0a52ac9147859da247b8d
    
    Signed-off-by: aswin7469 <aswinas@pixysos.com>
    Co-authored-by: CryllicBuster273 <cryllicbuster273@pixelexperience.org>
    Signed-off-by: aswin7469 <aswinas@pixysos.com>

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
 #
-# Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017-2020 The LineageOS Project
+# Copyright (C) 2018-2019 The LineageOS Project
 #
-# SPDX-License-Identifier: Apache-2.0
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 #
 
 set -e
+
+# Required!
+export DEVICE=avicii
+export VENDOR=oneplus
+
+export DEVICE_BRINGUP_YEAR=2020
 
 # Load extract_utils and do some sanity checks
 MY_DIR="${BASH_SOURCE%/*}"
@@ -21,28 +36,15 @@ if [ ! -f "${HELPER}" ]; then
 fi
 source "${HELPER}"
 
-# Initialize the helper for common
-setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${PIXYS_ROOT}" true
+# Initialize the helper for device
+INITIAL_COPYRIGHT_YEAR="$DEVICE_BRINGUP_YEAR"
+setup_vendor "${DEVICE}" "${VENDOR}" "${PIXYS_ROOT}" false
 
-# Warning headers and guards
-write_headers "avicii ebba"
+# Copyright headers and guards
+write_headers
 
-# The standard common blobs
+# The standard device blobs
 write_makefiles "${MY_DIR}/proprietary-files.txt" true
 
 # Finish
 write_footers
-
-if [ -s "${MY_DIR}/../${DEVICE}/proprietary-files.txt" ]; then
-    # Reinitialize the helper for device
-    setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false
-
-    # Warning headers and guards
-    write_headers
-
-    # The standard device blobs
-    write_makefiles "${MY_DIR}/../${DEVICE}/proprietary-files.txt" true
-
-    # Finish
-    write_footers
-fi
